50a982cc6f59e304db002898a879267236be28db
Maddog164 <denise@henderson3.us>
2020-12-16 07:40:47 -0500

    trouble shooting webpacker issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ require './config/environment'
 require "./app/models/user"
 
 class UsersController < ApplicationController
+
+    def create
+        User.create(user_params)
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation)
+    end
 end
