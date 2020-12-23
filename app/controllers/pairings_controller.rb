de71b3d210d6062aa3161ec5aaee79f908dbd64e
Maddog164 <denise@henderson3.us>
2020-12-23 06:18:25 -0500

    Continue work on forms build out esp nested forms

--- app/controllers/pairings_controller.rb
+++ app/controllers/pairings_controller.rb
@@ -3,17 +3,29 @@ require "./app/models/wine"
 
 class WinesController < ApplicationController
 
+    
+    
     def index
         if params[:wine_id]
             @pairings = Wine.find(params[:wine_id]).pairings
+            @wine = Wine.find(params[:wine_id])
         else
             @pairings = Pairing.all
         end
+    end
+
+    def create
+        Pairing.create(pairing_params)
     end
 
     def show
         @pairing = Pairing.find(params[:id])
     end
 
+    private
+
+    def pairing_params
+        params.require(:pairing).permit(:wine, wine_ids: [], :food, food_ids: [])
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
   end
 
   resources :foods
+
+  resources :pairings
   
 end
